020b0f5cb74432f1175b2a5bb366175addfbc683
Linard <linard.gauthier@gmail.com>
2016-12-10 17:07:08 +0100

    Add geometry

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Assistance:
     def __init__(self):
         self.root = Tk()
         self.root.title("Assistance de cuisine")
-        self.root.config(height=200)
+        self.root.geometry('800x220+0+0')
         self.root.configure(padx="10", pady="10")
 
         self.food_repository = FoodRepository()
